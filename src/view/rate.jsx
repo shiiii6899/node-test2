ce79da6afe2733c5b008a4c91415a7285a592ca9
Shiori Yasumoto <shiiii6899@gmail.com>
2020-11-10 17:32:33 +0900

    tbodyを加えた

--- src/view/rate.jsx
+++ src/view/rate.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 export default function Rate(props) {
   return (
     <table>
-    <tr>
-        <td><b>利率:</b></td>
-        <td>
-          <label><input type="number" name="number" value={props.rate} onChange={props.onChangeRate}/></label>
-        </td>
-      </tr>
+      <tbody>
+         <tr>
+          <td><b>利率:</b></td>
+          <td>
+            <label><input type="number" name="number" value={props.rate} onChange={props.onChangeRate}/></label>
+          </td>
+        </tr>
+      </tbody>
     </table>
   );
 }
--- src/view/return.jsx
+++ src/view/return.jsx
@@ -5,62 +5,66 @@ export default function Return(props) {
   <div className='main'>
     　<h2>リターン</h2>
     <table>
-      <tr>
-        <th>年</th>
-        <th>1</th>
-        <th>2</th>
-        <th>3</th>
-        <th>4</th>
-        <th>5</th>
+      <thead>
+        <tr>
+          <th>年</th>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+        </tr>
+      </thead>
 
-      </tr>
-      <tr>
-        <td>利確（払い戻し）6/15</td>
-        <td></td>
-        <td>
-        <label>
-            <input type="number"
-            name="number"
-            value={props.withdrawals[1]}
-            onChange={(event) => { props.onChangeWithdrawals(1, event) }}/>
-          </label>
-        </td>
-        <td>
-        <label>
-            <input type="number"
-            name="number"
-            value={props.withdrawals[2]}
-            onChange={(event) => { props.onChangeWithdrawals(2, event) }}/>
-          </label>
-        </td>
-        <td>
-        <label>
-            <input type="number"
-            name="number"
-            value={props.withdrawals[3]}
-            onChange={(event) => { props.onChangeWithdrawals(3, event) }}/>
-          </label>
-        </td>
-        <td>{props.final_withdrawals}</td>
-      </tr>
-      <tr>
-        <td>元本（払い戻し）3/30</td>
-        <td>
-          <label><input type="number" name="number" value={props.capital_withdrawal} onChange={props.onChangeCapital_withdrawal}/></label>
-        </td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>年間受取（払い戻し）</td>
-        <td>{props.capital_withdrawal}</td>
-        <td>{props.withdrawals[1]}</td>
-        <td>{props.withdrawals[2]}</td>
-        <td>{props.withdrawals[3]}</td>
-        <td>{props.final_withdrawals}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td>利確（払い戻し）6/15</td>
+          <td></td>
+          <td>
+          <label>
+              <input type="number"
+              name="number"
+              value={props.withdrawals[1]}
+              onChange={(event) => { props.onChangeWithdrawals(1, event) }}/>
+            </label>
+          </td>
+          <td>
+          <label>
+              <input type="number"
+              name="number"
+              value={props.withdrawals[2]}
+              onChange={(event) => { props.onChangeWithdrawals(2, event) }}/>
+            </label>
+          </td>
+          <td>
+          <label>
+              <input type="number"
+              name="number"
+              value={props.withdrawals[3]}
+              onChange={(event) => { props.onChangeWithdrawals(3, event) }}/>
+            </label>
+          </td>
+          <td>{props.final_withdrawals}</td>
+        </tr>
+        <tr>
+          <td>元本（払い戻し）3/30</td>
+          <td>
+            <label><input type="number" name="number" value={props.capital_withdrawal} onChange={props.onChangeCapital_withdrawal}/></label>
+          </td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>年間受取（払い戻し）</td>
+          <td>{props.capital_withdrawal}</td>
+          <td>{props.withdrawals[1]}</td>
+          <td>{props.withdrawals[2]}</td>
+          <td>{props.withdrawals[3]}</td>
+          <td>{props.final_withdrawals}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
   );
